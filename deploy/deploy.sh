6616b175b9592020aa7fbd0bbc532daa39e1f89b
wilmardo <info@wilmardenouden.nl>
2019-04-01 13:41:36 +0200

    chore: Update echo statement in deploy to echo correct tag

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Deploying ${DOCKER_NAMESPACE}/${CONTAINER_NAME}:${ARCH}"
+echo "Deploying ${DOCKER_NAMESPACE}/${CONTAINER_NAME}:${HOMEASSISTANT_VERSION}-${ARCH}"
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 docker push ${DOCKER_NAMESPACE}/${CONTAINER_NAME}
 
