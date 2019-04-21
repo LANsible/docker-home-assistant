1309b58fe56aa18fa59ad54c061691f1657f6061
wilmardo <info@wilmardenouden.nl>
2019-04-21 15:09:29 +0200

    chore: Refactor, more general setup

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Deploying ${DOCKER_NAMESPACE}/${CONTAINER_NAME}:${HOMEASSISTANT_VERSION}-${ARCH}"
+echo "Deploying ${DOCKER_NAMESPACE}/${CONTAINER_NAME}:${VERSION}-${ARCH}"
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 docker push ${DOCKER_NAMESPACE}/${CONTAINER_NAME}
 
@@ -7,16 +7,16 @@ echo "Install manifest-tool"
 wget https://github.com/estesp/manifest-tool/releases/download/v1.0.0-rc2/manifest-tool-linux-amd64 -O manifest-tool
 chmod +x manifest-tool
 
-echo "Deploying manifest for ${HOMEASSISTANT_VERSION}"
+echo "Deploying manifest for ${VERSION}"
 ./manifest-tool push from-args \
     --ignore-missing \
     --platforms linux/amd64,linux/arm64,linux/386 \
-    --template lansible/home-assistant:${HOMEASSISTANT_VERSION}-ARCH \
-    --target lansible/home-assistant:${HOMEASSISTANT_VERSION}
+    --template ${DOCKER_NAMESPACE}/${CONTAINER_NAME}:${VERSION}-ARCH \
+    --target ${DOCKER_NAMESPACE}/${CONTAINER_NAME}:${VERSION}
 
 echo "Deploying manifest for latest"
 ./manifest-tool push from-args \
     --ignore-missing \
     --platforms linux/amd64,linux/arm64,linux/386 \
-    --template lansible/home-assistant:${HOMEASSISTANT_VERSION}-ARCH \
-    --target lansible/home-assistant:latest
+    --template ${DOCKER_NAMESPACE}/${CONTAINER_NAME}:${VERSION}-ARCH \
+    --target ${DOCKER_NAMESPACE}/${CONTAINER_NAME}:latest
