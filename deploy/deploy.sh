22ed8055c5bbf619bc4ff6819b414679dd76a667
wilmardo <info@wilmardenouden.nl>
2019-04-21 14:02:35 +0200

    feat: More dynamic approach with tags on master

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,5 +7,14 @@ echo "Install manifest-tool"
 wget https://github.com/estesp/manifest-tool/releases/download/v1.0.0-rc2/manifest-tool-linux-amd64 -O manifest-tool
 chmod +x manifest-tool
 
-echo "Deploying manifest"
-./manifest-tool push from-spec manifests/manifest-${HOMEASSISTANT_VERSION}.yml
+echo "Deploying manifest for ${HOMEASSISTANT_VERSION}"
+./manifest-tool push from-args \
+    --platforms linux/amd64,linux/arm64,linux/386 \
+    --template lansible/home-assistant:${HOMEASSISTANT_VERSION}-ARCH \
+    --target lansible/home-assistant:${HOMEASSISTANT_VERSION}
+
+echo "Deploying manifest for latest"
+./manifest-tool push from-args \
+    --platforms linux/amd64,linux/arm64,linux/386 \
+    --template lansible/home-assistant:${HOMEASSISTANT_VERSION}-ARCH \
+    --target lansible/home-assistant:latest
